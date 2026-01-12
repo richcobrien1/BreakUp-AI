81282b072f5395d3e16c056ae35decbc4db5bc53
richcobrien <richcobrien@hotmail.com>
2026-01-11 19:32:10 -0700

    fix: Remove unused imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { SignedIn, SignedOut } from '@clerk/clerk-react'
 
 // Pages
 import LandingPage from './pages/LandingPage'
